64a11bbd73fec236b139eed209a3e594263479b0
sreyaascu <sreyaast@gmail.com>
2026-05-27 15:21:03 +0530

    The first version of Gomodoro Timer (Cause it was built using Go)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pomodoroV5
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+type Command int
+
+const (
+	Work  = "WORK"
+	Break = "BREAK"
+)
+
+const (
+	Pause Command = iota
+	Resume
+	Stop
+)
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+// Helper function
+func formatTime(secs int) (int, int) {
+	//Return min first and then seconds
+	return secs / 60, secs % 60
+}
+
+func playBeep() {
+	exec.Command(
+		"paplay",
+		"/usr/share/sounds/freedesktop/stereo/dialog-warning.oga",
+	).Run()
+}
+
+func runSession(label string, dur int, cmdChan chan Command, cyc int) bool {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	paused := false
+	status := "RUNNING"
+	for {
+		select {
+		case pipe := <-cmdChan:
+			switch pipe {
+			case Pause:
+				paused = true
+				status = "PAUSED"
+				// fmt.Println("\nTimer Paused")
+			case Resume:
+				// fmt.Println("\nResuming...")
+				status = "RUNNING"
+				paused = false
+			case Stop:
+				// fmt.Println("\nExiting...")
+				status = "STOPPED"
+				return true
+			}
+
+		case <-ticker.C:
+			clearScreen()
+			minute, second := formatTime(dur)
+
+			fmt.Println("====================================")
+			fmt.Println("             POMODORO              |")
+			fmt.Println("====================================")
+			if label == Work {
+				fmt.Printf("Session     :\033[31m%s\033[m                  |\n", label)
+			} else if label == Break {
+				fmt.Printf("Session     :\033[32m%s\033[m                 |\n", label)
+			}
+			fmt.Printf("Time        :%02d:%02d                 |\n", minute, second)
+			if len(strconv.Itoa(cyc)) == 1 {
+				fmt.Printf("Cycles      :%d                     |\n", cyc)
+			} else if len(strconv.Itoa(cyc)) == 2 {
+				fmt.Printf("Cycles:     :%d                    |\n", cyc)
+			} else if len(strconv.Itoa(cyc)) == 3 {
+				fmt.Printf("Cycles:     :%d                   |\n", cyc)
+			}
+			if status == "PAUSED" {
+				fmt.Printf("State       :\033[33m%s\033[m                |\n", status)
+			} else {
+				fmt.Printf("State       :\033[36m%s\033[m               |\n", status)
+			}
+			fmt.Println("====================================")
+			fmt.Print("Command (p=pause, r=resume, s=stop): ")
+			if paused {
+				continue
+			} else {
+				if dur >= 0 && dur < 5 {
+					go playBeep()
+				}
+				dur--
+				if dur < 0 {
+					return false
+				}
+			}
+
+		}
+	}
+}
+
+func runPomodoro(wrk int, brk int, cmdChan chan Command) {
+	cyc := 0
+	for {
+		if runSession(Work, wrk, cmdChan, cyc) {
+			return
+		}
+		if runSession(Break, brk, cmdChan, cyc) {
+			return
+		}
+		cyc++
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run . <work_time> <break_time>")
+		return
+	}
+	cmdChan := make(chan Command, 1)
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "start":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: go run . <work_time> <break_time>")
+			return
+		}
+
+		wrk_time, err1 := strconv.Atoi(os.Args[2])
+		brk_time, err2 := strconv.Atoi(os.Args[3])
+
+		if err1 != nil || err2 != nil {
+			fmt.Println("Invalid Input")
+			return
+		}
+		go func() {
+			var inp string
+			for {
+				fmt.Scanln(&inp)
+				switch inp {
+				case "p":
+					cmdChan <- Pause
+				case "r":
+					cmdChan <- Resume
+				case "s":
+					cmdChan <- Stop
+				}
+			}
+		}()
+		runPomodoro(wrk_time, brk_time, cmdChan)
+	}
+}
